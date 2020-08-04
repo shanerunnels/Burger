5c609dfeaf7ee817adaced66d1f1d77f1e6e505e
Shane Runnels <shanerunnels@gmail.com>
2020-08-04 17:59:19 -0500

    created burger.js file

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,21 @@
 const orm = require("../config/orm.js");
 
+let burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(burgerID, cb) {
+        orm.updateOne("burger", "devoured", burgerID, function(res) {
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger; 
