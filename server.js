46c6aee01880ab3129eea92e7c5ea63ac1215e8d
Shane Runnels <shanerunnels@gmail.com>
2020-08-05 16:15:39 -0500

    created server.js

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const express = require("express");
 
+let PORT = process.env.PORT || 8080;
+
+let app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
