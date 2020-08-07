8c7680dcedfce14154f302823f4597aa5c96b814
Shane Runnels <shanerunnels@gmail.com>
2020-08-07 15:47:44 -0500

    fixing heroku error

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
-const mysql = require("mysql");
+var mysql = require("mysql");
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "shanebogetem",
     database: "burgers_db"
-}); 
+  }); 
+};
 
-connection.connect(err => {
+
+connection.connect(function(err) {
     if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
+      console.error("error connecting: " + err.stack);
+      return;
     }
     console.log("connected as id " + connection.threadId);
 });
+
 
 module.exports = connection;
