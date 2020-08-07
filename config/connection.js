ee507b662d16de2f96647ad627450ab806618bfb
Shane Runnels <shanerunnels@gmail.com>
2020-08-07 15:09:32 -0500

    debugging heroku issues

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,12 @@
 const mysql = require("mysql");
 
-let connection; 
-
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else { mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "shanebogetem",
     database: "burgers_db"
-});
-}
-
-
+}); 
 
 connection.connect(err => {
     if (err) {
