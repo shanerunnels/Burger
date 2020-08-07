087ea4a91c95ad728a4d2d3484655bfb7cbd905a
Shane Runnels <shanerunnels@gmail.com>
2020-08-07 14:52:51 -0500

    added JawsDB to connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+let connection; 
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else { mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "shanebogetem",
     database: "burgers_db"
-}); 
+});
+}
+
+
 
 connection.connect(err => {
     if (err) {
--- config/orm.js
+++ config/orm.js
@@ -1,36 +1,88 @@
 const connection = require("../config/connection.js");
 
-let orm = {
-    selectAll: function(tableInput, cb) {
-        let queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-    insertOne: function(table, cols, vals, cb) {
-        let queryString = "INSERT INTO " + table;
-        
-        connection.query(queryString, [table, cols, vals], function(err, result) {
-            if (err) {
-                throw err;
-            }
+function printQuestionMarks(num) {
+  let arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
 
-            cb(result);
-        });
-    },
-    updateOne: function(table, updateColName, updateRowValue, searchColName, searchRowVal, cb) {
-        let queryString = "UPDATE " + table;
+function objToSql(ob) {
+  let arr = [];
 
-        connection.query(queryString, [table, updateColName, updateRowValue, searchColName, searchRowVal], function(err, res) {
-            if (err) {
-                throw err;
-            }
-            cb(res);
-        });
+  for (let key in ob) {
+    let value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
     }
+  }
+  return arr.toString();
+}
+
+// MySQL commands to select/add/update/delete
+let orm = {
+  selectAll: (tableInput, cb) => {
+    let queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: (table, cols, vals, cb) => {
+    let queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  updateOne: (table, objColVals, condition, cb) => {
+    let queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  delete: (table, condition, cb) => {
+    let queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
