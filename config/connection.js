a94673c3b7c90b1cc89342ab37b8b7701b6327dc
Shane Runnels <shanerunnels@gmail.com>
2020-08-07 17:46:31 -0500

    local host change

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 var mysql = require("mysql");
 
+var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
